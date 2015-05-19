30cd5eb54ca8927edb51de01734d3d2eab065a22
Eric Satterwhite <esatterwhite@wi.rr.com>
2015-05-19 00:42:51 -0500

    adding dirty math libs

---
+++ math.js
@@ -0,0 +1,94 @@
+/**
+ * Dirty Math lib for dealing with "64" bit integers.
+ * Actually tries to deal with number of an arbitrary length
+ * @module math
+ * @author Eric Satterwhite
+ **/
+
+function add( a, b ){
+	a = a.toString();
+	b = b.toString();
+	var carry   = 0;
+	var idx     = 0;
+	var sums    = [] 
+	var digit_a
+	var digit_b
+	
+	a = a.split('').reverse();
+	b = b.split('').reverse();
+
+	while( idx < a.length || idx < b.length || carry != 0 ){
+		digit_a = ~~a[idx];
+		digit_b = ~~b[idx]
+
+		sum = digit_a + digit_b + carry;
+
+		sums.push( (sum % 10).toString() )
+		carry = Math.floor( sum / 10 );
+		idx++;
+	}
+	
+	return sums.reverse().join('');	
+}
+
+function divide( a, b, remainder ){
+	a = String(a);
+	b = String(b);
+	var pos = b.length
+	var num = ~~a.substring( 0, pos );
+	var r = 0;
+	var next;
+
+	b = ~~b;
+	while( true ) {
+		if( b <= num ){
+			num -= b;
+			r++;
+		} else {
+			next = ( a.substring(pos, ++pos ));
+			if( next ){
+				num = ~~( num.toString() + next );
+				r *= 10
+			} else {
+				return remainder === true ? num : r
+			}
+		}
+	}
+}
+
+function mod( a, b ){
+	return divide( a,b, true );
+}
+
+function multiply( a, b ){
+
+	var carry    = 0;
+	var products = [];
+	var places = '';
+	var digit_a;
+	var digit_b;
+	var product;
+
+	a = a.split('').reverse();
+	b = b.split('').reverse();
+
+	while( b.length ){
+		digit_b = ~~b.shift()
+		product = places;
+		for( var idx = 0; idx < a.length; idx++ ){
+			digit_a = ~~a[idx];
+			var tmp = ( digit_b * digit_a + carry )	
+			carry = Math.floor( tmp/ 10 );
+			product = ( tmp % 10) + product;
+		}
+		products.push( ( carry || "" ).toString() + ( product ).toString() )
+		places += '0'
+		carry = 0;
+	}
+	return products.reduce( add );
+}
+
+exports.add = add;
+exports.mul = multiply;
+exports.div = divide;
+exports.mod = mod;
---
+++ test/math.spec.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+
+describe('dirty math',function(){
+	
+	it('should pass', function(){
+		assert.ok(1);
+	})
+})
